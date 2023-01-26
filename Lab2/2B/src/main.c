e92dad87c2caacd946e5f97deb4c1ddb9c1da391
Alexander Ward <alexanderward@ucsb.edu>
2023-01-26 12:00:32 -0800

    Finished lab2b

--- Lab2/2B/src/main.c
+++ Lab2/2B/src/main.c
@@ -12,20 +12,20 @@
 
 void System_Clock_Init(void) {
 	// Select MSI as system clock source
-	RCC->CFGR |= 0; // [TODO] - Replace with value that will make MSI the system clock source
+	RCC->CFGR |= RCC_CFGR_SW_MSI; // Replace with value that will make MSI the system clock source
 	
 	// Set MSI clock range
 	RCC->CR &= ~RCC_CR_MSIRANGE;
-	RCC->CR |= 0; // [TODO] - Replace with value that will make range 8 MHz
+	RCC->CR |= RCC_CR_MSIRANGE_7; //Value that will make range 8 MHz
 	
 	// Use the MSI clock range that is defined in RCC_CR
-	RCC->CR |= 0; // [TODO] - Replace with value that will select range in RCC->CR
+	RCC->CR |= RCC_CR_MSIRGSEL; // [Replace with value that will select range in RCC->CR
 	
 	// Enable MSI oscillator
-	RCC->CR |= 0; // [TODO] - Replace with value that will enable MSI
+	RCC->CR |= RCC_CR_MSION; // Replace with value that will enable MSI
 	
 	// Wait until MSI is ready
-	while((RCC->CR & 0 /* [TODO] - Replace with value that checks whether MSI is ready */) == 0);
+	while((RCC->CR & RCC_CR_MSIRDY /* Replace with value that checks whether MSI is ready */) == 0);
 }
 
 int main() {
@@ -35,6 +35,7 @@ int main() {
 	LED_Init();
 	
 	while(1) {
-		// [TODO] Write Simple Program here...
+		delay(1000);
+		Green_LED_Toggle();
 	}
 }
