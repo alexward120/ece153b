ed3a30efd2c1e84015d8ef3ec68817ac30224728
Diego Jerez <djerez5553@gmail.com>
2023-03-01 21:59:28 -0800

    Lab6 p2, only goes left

--- Lab6/L6B/src/main.c
+++ Lab6/L6B/src/main.c
@@ -10,26 +10,50 @@
 
 void SERVO_Pin_Init(){
 	// [TODO]
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODE0_1;
+	GPIOA->MODER &= ~GPIO_MODER_MODE0_0;
+	GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL0;
+	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL0_1;
+	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD0;
 }
 
 void TIM5_CH1_Init(){
 	// [TODO]
+	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM5EN;
+	TIM5->CR1 &= ~TIM_CR1_DIR;
+	TIM5->PSC = 39;
+	TIM5->ARR = 1999;
+	TIM5->CCMR1 &= ~TIM_CCMR1_OC1M;
+	TIM5->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
+	TIM5->CCMR1 |= TIM_CCMR1_OC1PE;
+	TIM5->CCER |= TIM_CCER_CC1E;
+	TIM5->CCR1 &= ~TIM_CCR1_CCR1;
+	TIM5->CCR1 = 999;
+	TIM5->CR1 |= TIM_CR1_CEN;
 }
 
 
 void Servo_Move_Left_90_Degree(){
 	// function to move the servo to +90 degrees
-	// [TODO]
+	// set duty cycle to 1 ms
+	TIM5->CCR1 &= ~TIM_CCR1_CCR1;
+	TIM5->CCR1 = 99;
+
 }
 
 void Servo_Move_Center(){
 	// function to move the servo to 0 degrees
-	// [TODO] 
+	TIM5->CCR1 &= ~TIM_CCR1_CCR1;
+	TIM5->CCR1 = 149;
 }
 
 void Servo_Move_Right_90_Degree(){
 	// function to move the servo to -90 degrees
-	// [TODO]
+	// set duty cycle to 2 ms
+	TIM5->CCR1 &= ~TIM_CCR1_CCR1;
+	TIM5->CCR1 = 210;
+
 }
 
 
@@ -42,7 +66,12 @@ int main() {
     while(1) {
 			// Move servo to +90 degrees, 0 degrees, and -90 degrees
 			// [TODO]
-			
-			for(i=0;i<1000;i++);  		// delay
+			Servo_Move_Left_90_Degree();
+			for(i=0;i<100000;i++);  		// delay
+			Servo_Move_Right_90_Degree();
+			for(i=0;i<100000;i++);  		// delay
+			Servo_Move_Center();
+			for(i=0;i<100000;i++);  		// delay
+
     }
 }
